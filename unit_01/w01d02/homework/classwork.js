3dbea62bda583f09f89a8651a3c54c8d7d004e6f
ejoysutton <joy.ericka.sutton@gmail.com>
2017-02-27 23:00:44 -0500

    Updates classwork.js from morning exercises

--- unit_01/w01d02/homework/classwork.js
+++ unit_01/w01d02/homework/classwork.js
@@ -9,61 +9,60 @@
 // by assigning the appropriate variable a value of either `true` or `false`.
 
 // 1. true || false 
-var resOne;
+var resOne  = true;
 
 // 2. null || ''
-var resTwo;
+var resTwo = false;
 
 // 3. 'false' && true
-var resThree;
+var resThree = true;
 
 // 4. !!0
-var resFour;
+var resFour = false;
 
 // 5. !null && !undefined
-var resFive;
+var resFive = true;
 
 /// Flow Control ///
 
 // 6. Write a `while` loop that increases `resSix` by 23 until `resSix` is five
 //    digits long. (Hint: take a look at the .toString() method)
 var resSix = 5;
-console.log(resSix.toString().length);
-
-while (resSix.toString().length < 5){
-  resSix+=23;
+while (reSix.toString().length  < 5) {
+  resSix += 23;
 }
-console.log(resSix)
 
 // 7. Write a `while` loop that increases `resSeven` by 12 when it's strictly
 //    below 95, decreases it by 7 when it's strictly above 105, and stops as
 //    soon as `resSeven` equals or falls between those two values.
 var resSeven = 65;
-
-do {
-  resSeven === 95 || 105; 95<resSeven< 105;
-}
-while (resSeven < 95){
-    resSeven =-7
-}
-while (resSeven > 105) {
-resSeven+=12
-}
-{
-  console.log(resSeven)
+while (resSeven > 105 || resSeven < 95) {
+  if (resSeven > 105) {
+    resSeven -= 7;
+  }
+  else if (resSeven < 95) {
+    resSeven += 12;
+  }
 }
 
 // 8. Write a `for` loop that counts from 0 (inclusive) to 1857 (exclusive) and
 //    calculates the sum of all of those numbers, storing the result in the
 //    variable `resEight`.
 var resEight;
-// Replace This Comment With Your Code
+  resEight = 0;
+  for (var i=0; i < 1857; i++) {
+        resEight += i;
+  }
+  
 
 // 9. Using a `for` loop, calculate the sum of all multiples of 7 between 0
 //    (inclusive) and 1000 (exclusive); store the result in the variable
 //    `resNine`.
 var resNine;
-// Replace This Comment With Your Code
+for (var i=0; <1000; i++) {
+    if i%7 === 0;
+    resNine += i;
+}
 
 // 10. For numbers 1-10, calculate n^n and then store the sum of all of these values in the variable `resTen`.
 var resTen;

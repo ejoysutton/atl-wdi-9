8457ca30ab527d36621d3e5284c1de8d5e30b5ad
ejoysutton <joy.ericka.sutton@gmail.com>
2017-03-01 20:11:48 -0500

    updated progress on independent-js-practice.

--- unit_01/w01d04/student_labs/js-independent-practice/independent-practice-starter.js
+++ unit_01/w01d04/student_labs/js-independent-practice/independent-practice-starter.js
@@ -4,6 +4,18 @@
 
 // your code here
 
+var lengths = function(arrayOfStrings) {
+	var arrayOfNumbers = [];
+	for(var i = 0; i < arrayOfStrings.length; i++) {
+		var lengthOfString = arrayOfStrings[i].length;
+		arrayOfNumbers.push(lengthOfString);
+	}
+	return arrayOfNumbers;
+};
+var arrayThing = ['one', 'two', 'three'];
+lengths(arrayThing);
+
+
 // 2. Write a Javascript function called `transmogrifier`
 // This function should accept three arguments, which you can assume will be numbers.
 // Your function should return the "transmogrified" result
@@ -11,10 +23,67 @@
 // The transmogrified result of three numbers is the product of the first two numbers,
 // raised to the power of the third number.
 
+
+
 // your code here
+
+var transmogrifier = function(base1, base2, exponent) {
+	var productOfBases = base1 * base2;
+	return Math.pow(productOfBases, exponent);
+};
+transmogrifier(1,2,3);
+
+
 
 // 3. Write a function `wordReverse` that accepts a single argument, a string. The
 // method should return a string with the order of the words reversed. Don't worry
 // about punctuation.
 
 // your code here
+// make function named wordReverse with 
+// parameter stringToReverse
+// turn string onto array of words 
+// create empty array to store words
+// working backwards through original array, place into empty array
+// return the result as a string
+
+var wordReverse = function(stringToReverse) {
+  var arrayOfWords = stringToReverse.split(" ");
+  var reverseWords = arrayOfWords.reverse();
+  return reverseWords.join(" ");
+};
+var someString="I've got a lovely bunch of coconuts"
+wordReverse(someString);
+
+OR
+
+var wordReverse = function(stringToReverse) {
+	var reversedString = stringToReverse
+		.split(' ')
+		.reverse()
+		.join(' ');
+	return reversedString;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

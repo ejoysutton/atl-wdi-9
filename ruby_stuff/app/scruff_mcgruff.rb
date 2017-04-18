ed5215ce3a0f39d6f360f99e0bef860a0cc000b0
ejoysutton <joy.ericka.sutton@gmail.com>
2017-04-17 22:25:30 -0400

    More 041717 ruby homework

---
+++ ruby_stuff/app/scruff_mcgruff.rb
@@ -0,0 +1,39 @@
+# Objectives
+# 1) Create a method that prints the value for the key "species"
+# 2) Create a method that will print the name and rating values in the bear_friend hash
+# 3) print the slogan defined in 'scruff'.  If it's not defined, then say "guess there's no slogan here!!"
+# 4) Method that puts the key, value pair if the value is "Scruff McGruff"
+
+require 'pry'
+# def initialize(scruffy)
+  @scruff = {
+  name: 'Scruff McGruff',
+  address: 'Chicago, IL 60652',
+  slogan: 'Help take a bite out of crime!',
+  species: 'Anthropomorphic animated dog',
+  birth_year: 1980,
+  communication_mediums: [
+    'television',
+    'newspaper',
+    'billboards',
+    'radio'
+  ],
+  bear_friend: {
+    name: 'Smokey the Bear',
+    rating: 10
+  },
+  owner: 'Jack Keil'
+}
+# end
+
+puts @scruff[:species]
+
+friend = @scruff[:bear_friend]
+puts "Here's his friend #{friend[:name]}, who he rates a #{friend[:rating]}"
+
+if @scruff[:slogan]
+  print @scruff[:slogan]
+else 
+  print "guess there's no slogan here!!"
+end
+
